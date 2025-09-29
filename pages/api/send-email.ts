44defe89fc407ab92896eb5eb95d6b42b27fe607
devbooth1-ui <devbooth1@gmail.com>
2025-09-29 17:19:22 -0400

    Change CORS policy to allow all origins

--- pages/api/send-email.ts
+++ pages/api/send-email.ts
@@ -3,7 +3,7 @@ import sgMail from '@sendgrid/mail';
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export default async function handler(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST,OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 

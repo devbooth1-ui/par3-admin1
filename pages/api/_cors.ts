f952c010b084aa7eb6f8e83c700b0909bd997a1a
Dev Booth <devbooth1@gmail.com>
2025-10-06 00:45:23 -0400

    Fix all type annotations in CORS middleware for successful build

--- pages/api/_cors.ts
+++ pages/api/_cors.ts
@@ -1,9 +1,14 @@
 import Cors from 'cors';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 // Helper to wait for a middleware to execute before continuing
-export function runMiddleware(req, res, fn) {
+export function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: (req: NextApiRequest, res: NextApiResponse, result: any) => void
+) {
   return new Promise((resolve, reject) => {
-    fn(req, res, (result) => {
+    fn(req, res, (result: any) => {
       if (result instanceof Error) {
         return reject(result);
       }
